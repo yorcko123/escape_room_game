8b76643a7cd697de250713f433c3788ef297325a
Kristin2101 <kristin.reinecke@t-online.de>
2025-11-27 18:01:05 +0100

    Update escape_room_game.py
    Garten Aktivität geschrieben

--- escape_room_game.py
+++ escape_room_game.py
@@ -20,13 +20,14 @@ pos_tod_x = 3
 # die folgende Karte funktioniert so: oben links in der Ecke startet es mit Index[0][0]
 karte_eg = [
     [
-    [".", ".", ".", ".", "."],
-    [".", ".", ".", ".", "."],
+    ["Garten", "Garten", "Terasse", "Büro", "Abstellkammer"],
+    ["Küche", "Küche", "Wohnzimmmer", "Wohnzimmer", "Abstellkammer"],
     [".", ".", ".", ".", "."],
     [".", ".", "Garderobe", "Treppe", "."],
     [".", ".", "Eingang", ".", "."],
     ["XX", "XX", "Ende", "XX", "XX"]
     ],
+    # Warum zwei Karten die identsich wirken?
     [
     [".", ".", ".", ".", "."],
     [".", "Küche", ".", "Speisekammer", "."],
@@ -85,10 +86,45 @@ def move():
 
 while True:
     move()
-    print ("Du befindest dich in "+karte_eg[etage_ich][pos_ich_y][pos_ich_x])
-
-if(karte_eg[etage_ich][pos_ich_y][pos_ich_x]):
     
+    # aktuelle Position wird gespeichert, alle drei Ebenen
+    ich_bin_in = karte_eg[etage_ich][pos_ich_y][pos_ich_x]
+
+    print ("Du befindest dich in "+ ich_bin_in)
+
+    #Wenn man in Garten angekommen ist, kann man mit der Umgebung interagieren
+    if(ich_bin_in=="Garten"):
+        print("Der Garten ist groß, möchtest du ihn noch weiter erkunden? So gehe nach westen")
+    
+    # an der Stelle [0][0][0] kann man was besonderes machen
+    if etage_ich==0 and pos_ich_x==0 and pos_ich_y ==0:
+        print ("Die Erde scheint an dieser Stelle weich zu sein. Vielleicht kann man hier as ausgfraben? Möchtest du graben?")
+        #das .lower() ist wichtig, damit sowohl ein groß als auch ein kleingeschriebenes Ja bzw. nein angenommen wird
+        graben = str(input("ja oder nein?").lower())
+        if(graben== "ja"):
+            print("Hmm, bisher wurde noch nichts gefunden, möchtest du weiter graben?") 
+            weitergraben = str(input("bist du neugiereig oder nicht? ja onder neine?").lower)
+            if(weitergraben=="ja"):
+                print("Irgendwie sehe ich immer noch nichts? Weitergraben ja oder nein?")
+                nochweiter= str(input("Immernoch weitergraben?"))
+            elif(weitergraben=="nein"):
+                print("Du schaufelst das gegrabene Loch wieder zu in gehst zurück zum Haus. Aus dem Augenwinkel siehst du kurz ein Flackern. Zwei gelbe Punkte, die deine jede Bewegung verfolgen")
+                if(nochweiter =="ja"):
+                    print("Du findest eine Kiste. Diese scheint echt groß zu sein... Woah ok, sie ist so groß wie du selsbt. Wenn du den morschen Deckel öffnest, quitscht dieser in einem unangenehmen hohen Ton, das man die Zähne zusammenbeißt. In der Kiste ist eine Person, Haut geöblicher als die von Frankensteins Monster. Sie ist weiblich und in ihren haöb offenen Mund erkennt man 3 goldene Zähne. Erschrocken lässt du den Deckel wieder fallen. Der Anblick kannst du nicht mehr vergessen")
+                if(nochweiter =="nein"):
+                    print("völlig außer Atem richtest du dich auf und schaust dir deine Umgebung an. Eigentlich recht hübsch im Garten. Hohe Bäume, große Blumenbeete mit Rosen, Tulpen, Vergissmeinnicht. Und noch vielen weitern Blumen, von denen man den Namen nicht kennt. Hochwertige Gartenmöbel. Der Rasen ist ziemlich hoch, es scheint so als hätte bis vor ein paar Monaten sich noch jemand um diesen Garten gekümmert. Dann gibt es noch ein paar Vogelhäuser und ein schwarzer Schatten, der durch dein Blickfeld huscht... warte was?")
+        elif(graben == "nein"):
+            print(" du gehst wieder Richtung Haus")
+
+
+
+
+
+
+        
+
+
+
 
 
 
