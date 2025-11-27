07f0a9681ff1b2fb003704a001f2e724aa41de93
yorcko123 <yorck.pius@googlemail.com>
2025-11-27 16:21:25 +0100

    Refactor escape room game with multi-floor support
    Updated player and death positions, added a second floor to the game map, and improved movement feedback.

--- escape_room_game.py
+++ escape_room_game.py
@@ -1,19 +1,31 @@
 import random
 
+#Position des Spielers
+etage_ich = 0
 pos_ich_y = 4
 pos_ich_x = 2
 
+#Position des Todes
+etage_tod = 0
 pos_tod_y = 3
 pos_tod_x = 3
 
-eg = True
-
 karte_eg = [
+    [
     [".", ".", ".", ".", "."],
     [".", ".", ".", ".", "."],
     [".", ".", ".", ".", "."],
     [".", ".", "Garderobe", "Treppe", "."],
-    [".", ".", "Eingang", ".", "."]
+    [".", ".", "Eingang", ".", "."],
+    ["XX", "XX", "Ende", "XX", "XX"]
+    ],
+    [
+    [".", ".", ".", ".", "."],
+    [".", "Küche", ".", "Speisekammer", "."],
+    [".", ".", ".", ".", "."],
+    [".", "Wohnzimmer", "Esszimmer", ".", "."],
+    [".", ".", ".", ".", "."]
+    ]
 ]
 
 def move():
@@ -27,19 +39,19 @@ def move():
     global pos_ich_y, pos_ich_x
     match richtung:
         case "nord":
-            print("du kek")
+            print("Du gehst richtung Norden.")
             pos_ich_y -= 1
 
         case "ost":
-            print("du kek")
+            print("Du gehst richtung Osten.")
             pos_ich_x += 1
 
         case "süd":
-            print("du kek")
+            print("Du gehst richtung Süden.")
             pos_ich_y += 1
 
         case "west":
-            print("du kek")
+            print("Du gehst richtung Westen.")
             pos_ich_x -= 1
 
         case _:
@@ -47,4 +59,4 @@ def move():
             print("Falsche Richtung")
 
 move()
-print (karte_eg[pos_ich_y][pos_ich_x])
+print (karte_eg[etage_ich][pos_ich_y][pos_ich_x])
